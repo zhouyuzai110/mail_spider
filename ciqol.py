a3f38d4c8d913a22be79aa4fa30f8df31d685a6c
saint-evas <zhouyuzai110@gmail.com>
2014-07-13 20:32:28 +0800

    2756263

--- ciqol.py
+++ ciqol.py
@@ -218,7 +218,7 @@ def main():
         t.setDaemon(True)
         t.start()
 
-    for i in range(1776692,3660000):
+    for i in range(2756263,3660000):
         try:
             visitUrl = "http://information.ciqol.com/buyernew/view/type/buyer/id/%d" %i
             queue.put(visitUrl)
